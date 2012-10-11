d46efb1b49237b3527e178922697f3b31a0ac44d
Michael Ore <selbram1993@gmail.com>
2012-10-11 13:24:58 -0500

    Created main file travesty.cpp

---
+++ travesty.cpp
@@ -0,0 +1,17 @@
+#include <istream>
+#include <map>
+
+using namespace travesty;
+
+int main(int argc, const char* argv[]) {
+	if (argc < 2) {
+		std::cout << "Usage: travesty n";
+		return;
+	}
+	int nparam = argv[1];
+	std::map<std::string, frequencymap> processed;
+	bool done = false;
+	while (!done) {
+		
+	}
+}
