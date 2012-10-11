abb8da8a5da3f772ab265151cfa0c062a5f97b38
Michael Ore <selbram1993@gmail.com>
2012-10-11 14:25:43 -0500

    Basic input processing structure done

--- travesty.cpp
+++ travesty.cpp
@@ -1,17 +1,38 @@
+#include <iostream>
 #include <istream>
+#include <ostream>
 #include <map>
+#include <queue>
 
-using namespace travesty;
+//using namespace travesty;
 
 int main(int argc, const char* argv[]) {
 	if (argc < 2) {
-		std::cout << "Usage: travesty n";
-		return;
+		std::cout << "Usage: travesty N\n";
+		return -1;
 	}
-	int nparam = argv[1];
-	std::map<std::string, frequencymap> processed;
-	bool done = false;
-	while (!done) {
-		
+	long nparam = strtol(argv[1], NULL, 10);
+	if (nparam < 1) {
+		std::cout << "N must be positive.\n";
 	}
+	std::map<std::queue<int>, std::map<int, long> > processed;
+	std::queue<int> ongoing;
+	std::queue<int> initial;
+	while (!std::cin.eof()) {
+		int nextChar = std::cin.get();
+		if (ongoing.size() < nparam-1) {
+			ongoing.push(nextChar);
+			initial.push(nextChar);
+		}
+		else {
+			processed[ongoing][nextChar]++;
+			ongoing.pop();
+			ongoing.push(nextChar);
+		}
+	}
+	std::map<std::queue<int>, std::map<int, long> >::iterator it;
+	for ( it=processed.begin() ; it != processed.end(); it++ ) {
+		std::cout << (*it).first.size() << " => " << (*it).second.size() << std::endl;
+	}
+	return 0;
 }
